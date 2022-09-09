25366c3220e734f32ba547b3c69c67f43f8e9672
root <root@windows10-udes.localdomain>
2022-09-09 23:49:53 +0000

    h

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	char num;
+
+	num = 'a';
+	
+	while (num <= 'z')
+	{
+		putchar(num);
+		num++;
+	}
+	num = 'A';
+	
+	while (num <= 'Z')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+
+
+}
