0affc806c3b33b26a60c9b09f9d4358e971c0b59
root <root@windows10-udes.localdomain>
2022-09-09 16:07:14 +0000

    mess

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,22 +7,22 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					if( n < 0 ) {
-						      /* if condition is true then print n is negative */
-								printf("%d is negative\n", n );
-								   } 
-						else if(n==0)  {
-							      /* if condition is true  print n is zero*/
-									printf("%d is zero\n", n );
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if ( n < 0 ) {
+	  /* if condition is true then print n is negative */
+		printf("%d is negative\n", n );
+	} 
+	else if (n==0)  {
+	  /* if condition is true  print n is zero*/
+		printf("%d is zero\n", n );
 									   }
-						    else
-							    	  /* if condition is false print n is positive*/
-							    		printf("%d is positive\n", n);
+	else
+	/* if condition is false print n is positive*/
+		printf("%d is positive\n", n);
 						    	
-						    	return (0);
+	return (0);
 }
