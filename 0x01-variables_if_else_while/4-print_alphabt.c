344bcf17bb4a97e95171da4177ae7909381e89e2
root <root@windows10-udes.localdomain>
2022-09-09 23:23:11 +0000

    h

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	    char num;
+
+
+
+	       num = 'a';
+
+
+
+	          while (num <= 'z')
+			      {
+				              if (num != 'q' && num != 'e')
+						              {
+								                  putchar(num);
+
+
+
+										         }
+					              num++;
+						          }
+		      putchar('\n');
+		          return (0);
+			      }
