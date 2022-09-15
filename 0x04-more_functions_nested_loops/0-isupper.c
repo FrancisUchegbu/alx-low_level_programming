fedc90afc2f1e4f1dffc2a0b6db7317aeeab0f22
root <root@windows10-udes.localdomain>
2022-09-15 19:34:34 +0000

    h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character
- * Return: Always 0.
+* _isupper - Check if paramter c represents an uppercase letter.
+* @c: Int representing a character
+* Return: Always 0.
 */
 int _isupper(int c)
 {
@@ -12,4 +12,4 @@ int _isupper(int c)
 		return (1);
 	}
 	return (0);
-}	
+}
