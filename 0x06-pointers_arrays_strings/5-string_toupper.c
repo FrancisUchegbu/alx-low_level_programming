cf553fd4338cdd5be13d7ec68897fcaa696cbd34
root <root@windows10-udes.localdomain>
2022-09-22 18:54:12 +0000

    h

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,13 +3,13 @@
 /**
 * string_toupper - lower to upper
 * @s: pointer to char params
-* Return: *s
+* Return: *
 */
 
 char *string_toupper(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
