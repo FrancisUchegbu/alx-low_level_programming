074d42c3ccbafc9fb2e148162cdfcafe8504f55c
root <root@windows10-udes.localdomain>
2022-09-26 21:08:29 +0000

    k

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,7 @@ void print_diagsums(int *a, int size)
 	int i;
 	unsigned int sum = 0, sum1 = 0;
 	
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
 		sum += a[(size * i) + i];
 		sum1 += a[(size * (i + 1)) - (i + 1)];
