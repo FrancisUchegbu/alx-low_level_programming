2be4ec3e08f9b73a90dda8ee0209f95761736ab6
root <root@windows10-udes.localdomain>
2022-09-26 18:27:12 +0000

    l

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes.
+* @dest: destination memory area.
+* @src: source memory area.
+* @n: bytes filled.
+* Return: the pointer to dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+	return (dest);
+}
