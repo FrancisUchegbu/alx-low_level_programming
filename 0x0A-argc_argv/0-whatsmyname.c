c3c7a4aca18401b7438ddd628a7f42382019581e
root <root@windows10-udes.localdomain>
2022-10-03 12:05:02 +0000

    l

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /** 
 * main - prints its name
 * @argc: the number of arguments passed.
 * @argv: the pointer to an array of pointers.
 * Return: Always 0
 */
-
 int main(int argc, char *argv[])
 {
 	while (argc--)
