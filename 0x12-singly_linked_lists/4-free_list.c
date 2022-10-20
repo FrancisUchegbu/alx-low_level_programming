84aab063f6f6c8c85a3df54c57343e89e7a83ad1
root <root@windows10-udes.localdomain>
2022-10-20 16:02:43 +0000

    l

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
 * free_list - free the memory allocated to a linked list
 * @head: a double pointer to a linked list
 * Return: nothing
-*/
+*/i
+
 void free_list(list_t *head)
 {
 	list_t *current, *next;
